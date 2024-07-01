03841516f1bd94d2f379905a06ee7eb506bd4e4d
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2024-06-30 20:35:53 -0400

    Add Docker config secret

---
+++ scripts/create-docker-config-secret.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -eu
+
+namespace="infra"
+
+secret_name="$1"
+config_json_file="$2"
+secret_yaml_file="$3"
+
+kubectl create secret generic "${secret_name}" \
+    --from-file=config.json="${config_json_file}" \
+    -o yaml \
+    --namespace="${namespace}" \
+    --dry-run=client \
+    | grep -v "\s*creationTimestamp:\s*null" > "${secret_yaml_file}"
