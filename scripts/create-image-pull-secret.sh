875662ebb2bf3928228bd2e6f6ca3c49bce89277
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2024-07-02 11:44:21 -0400

    Add image pull secret

---
+++ scripts/create-image-pull-secret.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -eu
+
+namespace="infra"
+
+secret_name="$1"
+config_json_file="$2"
+secret_yaml_file="$3"
+
+kubectl create secret generic "${secret_name}" \
+    --type="kubernetes.io/dockerconfigjson" \
+    --from-file=.dockerconfigjson="${config_json_file}" \
+    -o yaml \
+    --namespace="${namespace}" \
+    --dry-run=client \
+    | grep -v "\s*creationTimestamp:\s*null" > "${secret_yaml_file}"
