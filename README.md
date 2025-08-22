b97394153949d0f72bb30f6d8a0742ea8510d3b0
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2025-08-22 19:45:03 -0400

    Improve Docker secret creation

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 This is a Secret as Code project. Its purpose is to manage secrets in a simple but secure way. By so doing, secrets can be kept encrypted in the Git repository and their life cycles managed via pull requests. With this approach, we can take advantage of GitOps tools such as [Flux](https://fluxcd.io/flux/) which will decrypt the secrets and apply the decrypted secrets to a Kubernetes cluster for example.
 
+# Secret Creation
 Artifactory secrets can be created using a command similar to the below:
 ```
 scripts/create-artifactory-secret.sh <env> <system_name>
@@ -20,4 +21,9 @@ scripts/create-keycloak-secret.sh <env>
 Maven secrets can be created using a command similar to the below:
 ```
 scripts/create-maven-settings-secret.sh <env>
+```
+
+Docker secrets can be created using a command similar to the below:
+```
+scripts/create-docker-config-secret.sh <env>
 ```
--- scripts/create-docker-config-secret.sh
+++ scripts/create-docker-config-secret.sh
@@ -2,17 +2,70 @@
 
 set -eu
 
+script_dir=`realpath "$(dirname $0)"`
+project_dir=`realpath "${script_dir}/.."`
+template_dir=`realpath "${script_dir}/../templates/docker"`
+
 namespace="infra"
 
-secret_name="$1"
-config_json_file="$2"
-secret_yaml_file="$3"
+secret_name="docker-config"
 
-kubectl create secret generic "${secret_name}" \
-    --type="kubernetes.io/dockerconfigjson" \
-    --from-file=config.json="${config_json_file}" \
-    --from-file=.dockerconfigjson="${config_json_file}" \
-    -o yaml \
-    --namespace="${namespace}" \
-    --dry-run=client \
-    | grep -v "\s*creationTimestamp:\s*null" > "${secret_yaml_file}"
+get_credentials() {
+    local env="$1"
+
+    local credentials_dir="${NGUILAND_ARTIFACTORY_CONFIG_DIR}/jcr/admin"
+
+    local username=$(cat "${credentials_dir}/user")
+    local password=$(cat "${credentials_dir}/password")
+
+    printf "${username}:${password}"
+}
+
+create_secret() {
+    local env="$1"
+
+    local credentials=$(get_credentials "${env}")
+
+    local auth=`printf "${credentials}" | base64`
+
+    local config_json_template="${template_dir}/config-${env}.json"
+
+    local config_json=$(mktemp)
+
+    jq --arg auth "${auth}" '.auths[].auth=$auth' "${config_json_template}" > "${config_json}"
+
+    local secret_yaml="${project_dir}/${env}/sops-age/${secret_name}.yaml"
+
+    kubectl create secret generic "${secret_name}" \
+        --type="kubernetes.io/dockerconfigjson" \
+        --from-file=config.json="${config_json}" \
+        --from-file=.dockerconfigjson="${config_json}" \
+        -o yaml \
+        --namespace="${namespace}" \
+        --dry-run=client \
+        | grep -v "\s*creationTimestamp:\s*null" > "${secret_yaml}"
+
+    cd "${project_dir}"
+
+    sops -e -i "${secret_yaml}"
+
+    printf "The %s secret was created in the %s file" "${secret_name}" "${secret_yaml}"
+
+    rm "${config_json}"
+}
+
+main() {
+    local expected_arg_count=1
+
+    if [ "$#" -lt "${expected_arg_count}" ]; then
+        printf "Error: Not enough arguments provided.\n"
+        printf "Usage: $0 <env>\n"
+        exit 1
+    fi
+
+    local env="$1"
+
+    create_secret "${env}"
+}
+
+main "$@"
